829b8a6b96545600eab53151717eccce2a189505
Madison Werner <31974310+mwerner640@users.noreply.github.com>
2019-03-10 00:38:34 -0500

    Update printing_3b.py

--- printing_3b.py
+++ printing_3b.py
@@ -1,10 +1,16 @@
 #when in python, to show output on the console you should to include a print statement
 #you can directly include what you want to print, as below:
+
 print(5)
 print("look guys! I'm coding!")
+
 #or you can use variable names to print the variables:
+
 x = 26
 print(x)
+
 #you can also print both at the same time
+
 print(x+" cats")
+
 #try modifying this file or creating a new one to print "Hello HackBU!"
