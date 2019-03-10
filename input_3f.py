3403299b1d2b099efc91377a3e86b00cef1cffee
Madison Werner <31974310+mwerner640@users.noreply.github.com>
2019-03-10 00:54:45 -0500

    Create input_3f.py

---
+++ input_3f.py
@@ -0,0 +1,9 @@
+#for your program to be useful in a variety of situations it needs the ability to respond to user input
+#we do this with the line
+
+x = input("please type ")
+
+#the message in parenthesis will be displayed to the user and the data they enter will be saved to x
+#input can be called for at any time
+#write a function that accepts user input and a variable of your chosing 
+#then prints a result based on the interraction between the two
